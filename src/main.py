2e19ba242c3959e4127c6126a54d892b8eebaba3
Sawyer Bowerman <sbowerma@redhat.com>
2026-06-06 13:19:18 -0400

    update: fix ds get and tokenizer grab to not double quantize

--- src/main.py
+++ src/main.py
@@ -8,11 +8,11 @@ META_MODEL_ID = "meta-llama/Llama-3.2-1B-Instruct" # small decoder-only model
 QWEN_MODEL_ID = "Qwen/Qwen1.5-MoE-A2.7B" # small MoE model
 SCHEMES = ["W8A8", "W4A16", "FP8", "NVFP4"]
 #------------------------META------------------------#
-metaModel = AutoModelForCausalLM.from_pretrained(META_MODEL_ID, dtype="auto")
-tokenizer = AutoTokenizer.from_pretrained(META_MODEL_ID)
-ds = get_dataset(tokenizer)
-
 for SCHEME in SCHEMES:
+    metaModel = AutoModelForCausalLM.from_pretrained(META_MODEL_ID, dtype="auto")
+    metaTokenizer = AutoTokenizer.from_pretrained(META_MODEL_ID)
+    ds = get_dataset(metaTokenizer)
+
     metaRecipe = GPTQModifier(
         targets="Linear",
         scheme=SCHEME,
@@ -30,8 +30,7 @@ for SCHEME in SCHEMES:
     # Save to disk in compressed-tensors format.
     SAVE_DIR = "meta/" + META_MODEL_ID.split("/")[1] + f"-{SCHEME}"
     metaModel.save_pretrained(SAVE_DIR)
-    tokenizer.save_pretrained(SAVE_DIR)
-
+    metaTokenizer.save_pretrained(SAVE_DIR)
 #----------------------------------------------------#
 
 
