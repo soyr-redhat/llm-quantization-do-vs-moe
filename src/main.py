cd1871b01f919ddb48d6748eae71787cefbc83e5
Sawyer Bowerman <sbowerma@redhat.com>
2026-06-06 13:15:35 -0400

    update: format for multi-runs and add quant

--- src/main.py
+++ src/main.py
@@ -1,22 +1,38 @@
 from transformers import AutoModelForCausalLM, AutoTokenizer
 from llmcompressor import oneshot
-from llmcompressor.modifiers.quantization import GPTQModifier
+from llmcompressor.modifiers.gptq import GPTQModifier
 from compressed_tensors.offload import dispatch_model
+from tokenize_dataset import get_dataset, MAX_SEQUENCE_LENGTH, NUM_CALIBRATION_SAMPLES
 
 META_MODEL_ID = "meta-llama/Llama-3.2-1B-Instruct" # small decoder-only model
 QWEN_MODEL_ID = "Qwen/Qwen1.5-MoE-A2.7B" # small MoE model
-
+SCHEMES = ["W8A8", "W4A16", "FP8", "NVFP4"]
 #------------------------META------------------------#
 metaModel = AutoModelForCausalLM.from_pretrained(META_MODEL_ID, dtype="auto")
 tokenizer = AutoTokenizer.from_pretrained(META_MODEL_ID)
+ds = get_dataset(tokenizer)
 
-metaRecipe = 
-# Apply quantization.
-oneshot(model=metaModel, recipe=metaRecipe)
+for SCHEME in SCHEMES:
+    metaRecipe = GPTQModifier(
+        targets="Linear"
+        scheme=SCHEME,
+        ignore=["lm_head"],
+    )
+    # Apply quantization.
+    oneshot(
+        model=metaModel,
+        dataset=ds,
+        recipe=metaRecipe,
+        max_seq_length=MAX_SEQUENCE_LENGTH,
+        num_calibration_samples=NUM_CALIBRATION_SAMPLES
+    )
 
-# Save to disk in compressed-tensors format.
-SAVE_DIR = META_MODEL_ID.split("/")[1] + "-FP8-BLOCK"
-metaModel.save_pretrained(SAVE_DIR)
-tokenizer.save_pretrained(SAVE_DIR)
+    # Save to disk in compressed-tensors format.
+    SAVE_DIR = META_MODEL_ID.split("/")[1] + "-FP8-BLOCK"
+    metaModel.save_pretrained(SAVE_DIR)
+    tokenizer.save_pretrained(SAVE_DIR)
 
 #----------------------------------------------------#
+
+
+
