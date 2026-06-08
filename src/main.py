0f69fd10a9b74467cd831a091226b6accb5f7586
Sawyer Bowerman <sbowerma@redhat.com>
2026-06-08 19:17:09 -0400

    update: remove commented code from main.py

--- src/main.py
+++ src/main.py
@@ -6,37 +6,37 @@ from tokenize_dataset import get_dataset, MAX_SEQUENCE_LENGTH, NUM_CALIBRATION_S
 
 META_MODEL_ID = "meta-llama/Llama-3.2-1B-Instruct" # small decoder-only model
 DEEPSEEK_MODEL_ID = "deepseek-ai/deepseek-moe-16b-chat" # small MoE model
-# SCHEMES = ["W8A8", "W4A16", "FP8", "NVFP4"]
-SCHEMES = ["NVFP4"]
-# metaTokenizer = AutoTokenizer.from_pretrained(META_MODEL_ID)
-# mDs = get_dataset(metaTokenizer)
+SCHEMES = ["W8A8", "W4A16", "FP8", "NVFP4"]
+
+metaTokenizer = AutoTokenizer.from_pretrained(META_MODEL_ID)
+mDs = get_dataset(metaTokenizer)
 
 dsTokenizer = AutoTokenizer.from_pretrained(DEEPSEEK_MODEL_ID, trust_remote_code=True)
 dsDs = get_dataset(dsTokenizer)
 
 #------------------------META------------------------#
 for SCHEME in SCHEMES:
-#     metaModel = AutoModelForCausalLM.from_pretrained(META_MODEL_ID, dtype="auto")
+    metaModel = AutoModelForCausalLM.from_pretrained(META_MODEL_ID, dtype="auto")
     
-#     metaRecipe = GPTQModifier(
-#         targets="Linear",
-#         scheme=SCHEME,
-#         ignore=["lm_head"],
-#     )
+    metaRecipe = GPTQModifier(
+        targets="Linear",
+        scheme=SCHEME,
+        ignore=["lm_head"],
+    )
 
-#     # Apply quantization.
-#     oneshot(
-#         model=metaModel,
-#         dataset=mDs,
-#         recipe=metaRecipe,
-#         max_seq_length=MAX_SEQUENCE_LENGTH,
-#         num_calibration_samples=NUM_CALIBRATION_SAMPLES
-#     )
+    # Apply quantization.
+    oneshot(
+        model=metaModel,
+        dataset=mDs,
+        recipe=metaRecipe,
+        max_seq_length=MAX_SEQUENCE_LENGTH,
+        num_calibration_samples=NUM_CALIBRATION_SAMPLES
+    )
 
-#     # Save to disk in compressed-tensors format.
-#     SAVE_DIR = "/output/" + "meta/" + META_MODEL_ID.split("/")[1] + f"-{SCHEME}"
-#     metaModel.save_pretrained(SAVE_DIR)
-#     metaTokenizer.save_pretrained(SAVE_DIR)
+    # Save to disk in compressed-tensors format.
+    SAVE_DIR = "/output/" + "meta/" + META_MODEL_ID.split("/")[1] + f"-{SCHEME}"
+    metaModel.save_pretrained(SAVE_DIR)
+    metaTokenizer.save_pretrained(SAVE_DIR)
 
 #---------------------DEEPSEEK-----------------------#
     dsModel = AutoModelForCausalLM.from_pretrained(DEEPSEEK_MODEL_ID, dtype="auto", trust_remote_code=True)
